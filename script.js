d050a6d8d2b64d2fad63728dd2993ba599e84956
Marek Skolik <marek250296@interia.pl>
2023-07-12 10:53:50 +0200

    added edit employee

--- script.js
+++ script.js
@@ -1,40 +1,38 @@
 $(document).ready(function() {
-  $('#tabela-pracownikow').DataTable({
-    language: {
-      url: 'https://cdn.datatables.net/plug-ins/1.11.3/i18n/Polish.json'
-    },
+  $('#employee-table').DataTable({
     columnDefs: [
-      { targets: [0], visible: false }
+    { targets: [0], visible: false }
     ],
     order: [[2, 'asc']]
-  });
-
-  $('#pracownik-form').submit(function(event) {
-    event.preventDefault();
-
-    var imie = $('#imie').val();
-    var nazwisko = $('#nazwisko').val();
-    var stanowisko = $('#stanowisko').val();
-    var filia = $('#filia').val();
-    var dataRozpoczecia = $('#data-rozpoczecia').val();
-    var zarobki = $('#zarobki').val();
-
-    var table = $('#tabela-pracownikow').DataTable();
-    table.row.add([
-      imie,
-      nazwisko,
-      stanowisko,
-      filia,
-      dataRozpoczecia,
-      zarobki,
+    });
+    
+    $('#employee-form').submit(function(event) {
+      event.preventDefault();
+    
+      var firstName = $('#first-name').val();
+      var lastName = $('#last-name').val();
+      var position = $('#position').val();
+      var branchLocation = $('#branch-location').val();
+      var startDate = $('#start-date').val();
+      var salary = $('#salary').val();
+        
+      var table = $('#employee-table').DataTable();
+      table.row.add([
+      '',
+      firstName,
+      lastName,
+      position,
+      branchLocation,
+      startDate,
+      salary,
       '<button class="edit-btn">Edytuj</button><button class="delete-btn">Usuń</button>'
     ]).draw();
 
-    $('#pracownik-form')[0].reset();
+  $('#employee-form')[0].reset();
   });
 
-  $('#eksport-csv').click(function() {
-    var table = $('#tabela-pracownikow').DataTable();
+  $('#export-csv').click(function() {
+    var table = $('#employee-table').DataTable();
     var data = table
       .rows()
       .data()
@@ -47,58 +45,59 @@ $(document).ready(function() {
     var link = document.createElement('a');
     var url = URL.createObjectURL(blob);
     link.setAttribute('href', url);
-    link.setAttribute('download', 'lista_pracownikow.csv');
+    link.setAttribute('download', 'employee_list.csv');
     link.style.visibility = 'hidden';
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
   });
 
-  $('#tabela-pracownikow').on('click', '.edit-btn', function() {
-    var table = $('#tabela-pracownikow').DataTable();
-    var row = $(this).closest('tr');
-    var data = table.row(row).data();
+    $('#employee-table').on('click', '.edit-btn', function() {
+        var table = $('#employee-table').DataTable();
+        var row = $(this).closest('tr');
+        var data = table.row(row).data();
 
-    $('#pracownik-id').val(data[0]);
-    $('#edycja-imie').val(data[1]);
-    $('#edycja-nazwisko').val(data[2]);
-    $('#edycja-stanowisko').val(data[3]);
-    $('#edycja-filia').val(data[4]);
-    $('#edycja-data-rozpoczecia').val(data[5]);
-    $('#edycja-zarobki').val(data[6]);
+        $('#employee-id').val(data[0]);
+        $('#edit-first-name').val(data[1]);
+        $('#edit-last-name').val(data[2]);
+        $('#edit-position').val(data[3]);
+        $('#edit-branch-location').val(data[4]);
+        $('#edit-start-date').val(data[5]);
+        $('#edit-salary').val(data[6]);
+        
+        $('#edit-modal').modal('show');
+    });
 
-    $('#modal-edycja').modal('show');
-  });
+    $('#save-edits').click(function() {
+      var id = $('#employee-id').val();
+      var firstName = $('#edit-first-name').val();
+      var lastName = $('#edit-last-name').val();
+      var position = $('#edit-position').val();
+      var branchLocation = $('#edit-branch-location').val();
+      var startDate = $('#edit-start-date').val();
+      var salary = $('#edit-salary').val();
+  
+      var table = $('#employee-table').DataTable();
+      var row = table.row('#' + id);
+  
+      row.data([
+        id,
+        firstName,
+        lastName,
+        position,
+        branchLocation,
+        startDate,
+        salary,
+        '<button class="edit-btn">Edytuj</button><button class="delete-btn">Usuń</button>'
+      ]).draw();
+  
+      $('#edit-modal').modal('hide');
+    });
+  
 
-  $('#zapisz-edycje').click(function() {
-    var id = $('#pracownik-id').val();
-    var imie = $('#edycja-imie').val();
-    var nazwisko = $('#edycja-nazwisko').val();
-    var stanowisko = $('#edycja-stanowisko').val();
-    var filia = $('#edycja-filia').val();
-    var dataRozpoczecia = $('#edycja-data-rozpoczecia').val();
-    var zarobki = $('#edycja-zarobki').val();
-
-    var table = $('#tabela-pracownikow').DataTable();
-    var row = table.row('#' + id);
-
-    row.data([
-      id,
-      imie,
-      nazwisko,
-      stanowisko,
-      filia,
-      dataRozpoczecia,
-      zarobki,
-      '<button class="edit-btn">Edytuj</button><button class="delete-btn">Usuń</button>'
-    ]).draw();
-
-    $('#modal-edycja').modal('hide');
-  });
-
-  $('#tabela-pracownikow').on('click', '.delete-btn', function() {
-    var table = $('#tabela-pracownikow').DataTable();
-    var row = $(this).closest('tr');
-    table.row(row).remove().draw();
-  });
-});
+      $('#employee-table').on('click', '.delete-btn', function() {
+        var table = $('#employee-table').DataTable();
+        var row = $(this).closest('tr');
+        table.row(row).remove().draw();
+      });
+    });
